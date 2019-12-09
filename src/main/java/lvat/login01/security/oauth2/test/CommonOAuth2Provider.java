b6a2a3716c13f4f550fc02017b49dc91d38f2292
levananhtu <levananhtu00@gmail.com>
2019-12-10 00:27:15 +0700

    checkpoint 05
    some thing new

---
+++ src/main/java/lvat/login01/security/oauth2/test/CommonOAuth2Provider.java
@@ -0,0 +1,120 @@
+package lvat.login01.security.oauth2.test;
+
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+
+public enum CommonOAuth2Provider {
+
+    GOOGLE {//https://accounts.google.com/.well-known/openid-configuration
+
+        @Override
+        public ClientRegistration.Builder getBuilder(String registrationId) {
+            ClientRegistration.Builder builder = getBuilder(registrationId,
+                    ClientAuthenticationMethod.BASIC, DEFAULT_REDIRECT_URL);
+            builder.scope("openid", "profile", "email");
+            builder.authorizationUri("https://accounts.google.com/o/oauth2/v2/auth");
+            builder.tokenUri("https://www.googleapis.com/oauth2/v4/token");
+            builder.jwkSetUri("https://www.googleapis.com/oauth2/v3/certs");
+            builder.userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo");
+            builder.userNameAttributeName(IdTokenClaimNames.SUB);
+            builder.clientName("Google");
+            return builder;
+        }
+    },
+
+    GITHUB {
+        @Override
+        public ClientRegistration.Builder getBuilder(String registrationId) {
+            ClientRegistration.Builder builder = getBuilder(registrationId,
+                    ClientAuthenticationMethod.BASIC, DEFAULT_REDIRECT_URL);
+            builder.scope("read:user");
+            builder.authorizationUri("https://github.com/login/oauth/authorize");
+            builder.tokenUri("https://github.com/login/oauth/access_token");
+            builder.userInfoUri("https://api.github.com/user");
+            builder.userNameAttributeName("id");
+            builder.clientName("GitHub");
+            return builder;
+        }
+    },
+
+    GITLAB {//https://gitlab.com/.well-known/openid-configuration
+
+        @Override
+        public ClientRegistration.Builder getBuilder(String registrationId) {
+            ClientRegistration.Builder builder = getBuilder(registrationId,
+                    ClientAuthenticationMethod.BASIC, DEFAULT_REDIRECT_URL);
+            builder.authorizationUri("https://gitlab.com/oauth/authorize");
+            builder.tokenUri("https://gitlab.com/oauth/token");
+            builder.userInfoUri("https://gitlab.com/oauth/userinfo");
+            builder.jwkSetUri("https://gitlab.com/oauth/discovery/keys");
+            builder.scope("email");
+            builder.clientName("GitLab");
+            return builder;
+        }
+    },
+
+    FACEBOOK {
+        @Override
+        public ClientRegistration.Builder getBuilder(String registrationId) {
+            ClientRegistration.Builder builder = getBuilder(registrationId,
+                    ClientAuthenticationMethod.POST, DEFAULT_REDIRECT_URL);
+            builder.scope("public_profile", "email");
+            builder.authorizationUri("https://www.facebook.com/v2.8/dialog/oauth");
+            builder.tokenUri("https://graph.facebook.com/v2.8/oauth/access_token");
+            builder.userInfoUri("https://graph.facebook.com/me?fields=id,name,email");
+            builder.userNameAttributeName("id");
+            builder.clientName("Facebook");
+            return builder;
+        }
+    },
+
+    MICROSOFT {//https://login.microsoftonline.com/contoso.onmicrosoft.com/.well-known/openid-configuration
+        @Override
+        public ClientRegistration.Builder getBuilder(String registrationId) {
+            ClientRegistration.Builder builder = getBuilder(registrationId,
+                    ClientAuthenticationMethod.BASIC, DEFAULT_REDIRECT_URL);
+            builder.scope("email");
+            builder.authorizationUri("https://login.microsoftonline.com/31537af4-6d77-4bb9-a681-d2394888ea26/oauth2/authorize");
+            builder.tokenUri("https://login.microsoftonline.com/31537af4-6d77-4bb9-a681-d2394888ea26/oauth2/token");
+            builder.jwkSetUri("https://login.microsoftonline.com/common/discovery/keys");
+            builder.userInfoUri("https://login.microsoftonline.com/31537af4-6d77-4bb9-a681-d2394888ea26/openid/userinfo");
+            return builder;
+        }
+    },
+
+    OKTA {
+        @Override
+        public ClientRegistration.Builder getBuilder(String registrationId) {
+            ClientRegistration.Builder builder = getBuilder(registrationId,
+                    ClientAuthenticationMethod.BASIC, DEFAULT_REDIRECT_URL);
+            builder.scope("openid", "profile", "email");
+            builder.userNameAttributeName(IdTokenClaimNames.SUB);
+            builder.clientName("Okta");
+            return builder;
+        }
+    };
+
+    private static final String DEFAULT_REDIRECT_URL = "{baseUrl}/{action}/oauth2/code/{registrationId}";
+
+    protected final ClientRegistration.Builder getBuilder(String registrationId,
+                                                          ClientAuthenticationMethod method, String redirectUri) {
+        ClientRegistration.Builder builder = ClientRegistration.withRegistrationId(registrationId);
+        builder.clientAuthenticationMethod(method);
+        builder.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE);
+        builder.redirectUriTemplate(redirectUri);
+        return builder;
+    }
+
+    /**
+     * Create a new
+     * {@link org.springframework.security.oauth2.client.registration.ClientRegistration.Builder
+     * ClientRegistration.Builder} pre-configured with provider defaults.
+     *
+     * @param registrationId the registration-id used with the new builder
+     * @return a builder instance
+     */
+    public abstract ClientRegistration.Builder getBuilder(String registrationId);
+
+}
---
+++ src/main/java/lvat/login01/security/oauth2/test/OAuth2LoginConfig.java
@@ -0,0 +1,51 @@
+package lvat.login01.security.oauth2.test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+
+@Configuration
+public class OAuth2LoginConfig {
+
+    @EnableWebSecurity
+    public static class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2Login();
+        }
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId("google-client-id")
+                .clientSecret("google-client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("openid", "profile", "email", "address", "phone")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .clientName("Google")
+                .build();
+    }
+}
